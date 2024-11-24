4ceade35a81d1b47283872a60a1c21d4a48e7b9d
bot <info@devopsinsiders.com>
2024-11-24 11:38:55 +0530

    Fixed comments in PR

--- modules/azurerm-aks/main.tf
+++ modules/azurerm-aks/main.tf
@@ -1,37 +1,23 @@
-
-resource "azurerm_kubernetes_cluster" "dev_aks" {
-    for_each = var.aks_var
-  name                = each.value.aks_name          # name of the kubernetes cluster #
+# Block for Kubernetes Cluster
+resource "azurerm_kubernetes_cluster" "aks" {
+  for_each            = var.aks_clusters
+  name                = each.value.aks_name
   location            = each.value.location
   resource_group_name = each.value.rg_name
-  dns_prefix          = "devaks1"
-  
+  dns_prefix          = each.value.dns_prefix
 
   default_node_pool {
     name       = "default"
-    node_count = 2
-    vm_size    = "Standard_D2_v2"
+    node_count = each.value.node_count
+    vm_size    = each.value.vm_size
   }
 
   identity {
     type = "SystemAssigned"
   }
-network_profile {
-  network_plugin = "azure"          
-  network_policy = "calico"             
-}
-  tags = {
-    Environment = "dev"
+  network_profile {
+    network_plugin = "azure"
+    network_policy = "calico"
   }
+  tags = each.value.tags
 }
-
-# output "client_certificate" {
-#   value     = azurerm_kubernetes_cluster.example.kube_config[0].client_certificate
-#   sensitive = true
-# }
-
-# output "kube_config" {
-#   value = azurerm_kubernetes_cluster.example.kube_config_raw
-
-#   sensitive = true
-# }
--- modules/azurerm-aks/variable.tf
+++ modules/azurerm-aks/variable.tf
@@ -1,3 +1,13 @@
-variable "aks_var" {
-  type = map(any)
+variable "aks_clusters" {
+  description = "A map of AKS cluster configurations."
+  type = map(object({
+    aks_name       = string
+    location       = string
+    rg_name        = string
+    dns_prefix     = string
+    node_count     = number
+    vm_size        = string
+    tags           = map(string)
+  }))
+  default = {}
 }
