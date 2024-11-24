9f47b56aa335f517796e0e320c2df612ef11e2f0
bot <info@devopsinsiders.com>
2024-11-24 12:19:30 +0530

    update code

--- modules/azurerm-acr/main.tf
+++ modules/azurerm-acr/main.tf
@@ -1,8 +1,8 @@
 resource "azurerm_container_registry" "acr" {
-    for_each = var.acrs
+  for_each            = var.acrs
   name                = each.value.acr_name
-  resource_group_name =  each.value.rg_name
+  resource_group_name = each.value.rg_name
   location            = each.value.location
-  sku                 = "Premium"
+  sku                 = each.value.sku
   admin_enabled       = false
 }
